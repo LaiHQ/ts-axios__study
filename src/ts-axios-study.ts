43e713860a6ff8d0761ffd8497881b2aedae2fa0
lai_hq@126.com <lai_hq@126.com>
2022-06-12 14:04:58 +0800

    init

--- src/ts-axios-study.ts
+++ src/ts-axios-study.ts
@@ -1,4 +1,10 @@
+/*
+ * @Date: 2022-06-12 13:17:16
+ * @LastEditors: lai_hq@qq.com
+ * @LastEditTime: 2022-06-12 14:04:26
+ * @FilePath: \apph5f:\code\typescript-library-starter\src\ts-axios-study.ts
+ */
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class DummyClass {}
+export default class DummyClass {}   
